4896ebb75dc7f19c344b28630bd8a5fe747d71f5
K.Makhmudov <komiljonmakhmudov76@gmail.com>
2026-06-01 10:41:46 +0500

    test commit

--- src/main/java/com/javarush/makhmudov/MainRunner.java
+++ src/main/java/com/javarush/makhmudov/MainRunner.java
@@ -3,6 +3,6 @@ package com.javarush.makhmudov;
 public class MainRunner {
     public static void main(String[] args) {
 //test
-        System.out.println("Hello maven");
+        System.out.println("Hello maven!");
     }
 }
