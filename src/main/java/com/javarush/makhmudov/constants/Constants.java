32be0c2a3dd0ede023ef08820dd0124a3b9fc695
K.Makhmudov <komiljonmakhmudov76@gmail.com>
2026-06-01 12:58:14 +0500

    add Text

--- src/main/java/com/javarush/makhmudov/constants/Constants.java
+++ src/main/java/com/javarush/makhmudov/constants/Constants.java
@@ -1,9 +1,14 @@
 package com.javarush.makhmudov.constants;
 
+import java.io.File;
+import java.nio.file.Files;
+
 public class Constants {
     private static final String rus = "ЙЦУКЕНГШЩЗХЪЭЖДЛОРПАВЫФЯЧСМИТЬБЮ";
     private static final String eng = "QWERTYUIOPLKJHGFDSAZXCVBNM";
     private static final String son = "0123456789";
     private static final String belgi = "!@#$%^&*()_+|-=\\[]{}';:/.,<>?";
+
     private static final String ALPHABET = rus + eng + rus.toLowerCase() + eng.toLowerCase() + son + belgi;
+    public static final String TXT_FOLDER=System.getProperty("user.dir")+ File.separator+"text"+File.separator;
 }
