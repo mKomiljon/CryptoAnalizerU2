7ffd962800a2b3c8df2fd19641ff52c6c89ef54a
K.Makhmudov <komiljonmakhmudov76@gmail.com>
2026-06-02 16:00:21 +0500

    BruteForce tuzatildi

--- src/main/java/com/javarush/makhmudov/commands/BruteForce.java
+++ src/main/java/com/javarush/makhmudov/commands/BruteForce.java
@@ -3,10 +3,10 @@ package com.javarush.makhmudov.commands;
 import com.javarush.makhmudov.entity.Result;
 import com.javarush.makhmudov.entity.ResultCode;
 
-public class BrudeForce implements Action {
+public class BruteForce implements Action {
     @Override
     public Result execute(String[] parameters) {
         //TODO something do
-        return new Result("BrudeForce complete", ResultCode.OK);
+        return new Result("BruteForce complete", ResultCode.OK);
     }
 }
