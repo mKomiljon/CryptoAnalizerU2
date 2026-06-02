13585835f52076e152f74382b5b2eb9820b8f528
K.Makhmudov <komiljonmakhmudov76@gmail.com>
2026-06-02 15:28:00 +0500

    Menu qushildi

--- src/main/java/com/javarush/makhmudov/MainRunner.java
+++ src/main/java/com/javarush/makhmudov/MainRunner.java
@@ -1,10 +1,16 @@
 package com.javarush.makhmudov;
 
+
+import com.javarush.makhmudov.commands.Menu;
 import com.javarush.makhmudov.entity.Result;
+
+import java.util.Scanner;
 
 public class MainRunner {
     public static void main(String[] args) {
-        //encode text.txt encode.txt 12
+        //build console app
+        Scanner input = new Scanner(System.in);
+        Menu menu = new Menu(input);
         Application application = new Application();
         Result result = application.run(args);
         System.out.println(result);
